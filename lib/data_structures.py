f16c42c8078339d0e9cd05e83e1a926bd5ac8a2b
wachuka7 <rachaelwachuka7@gmail.com>
2024-03-03 23:50:06 +0300

    feat: add functions

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,10 +17,10 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return [spicy_food['name'] for spicy_food in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food['heat_level']>5]
 
 def print_spicy_foods(spicy_foods):
     pass
